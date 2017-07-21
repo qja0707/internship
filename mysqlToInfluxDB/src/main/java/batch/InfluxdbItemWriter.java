4cacb9ce0739fa416d4bc91c2482e973512b7b99
gyubeom.park <gyubeom.park@navercorp.com>
2017-07-21 18:40:55 +0900

    read-write version / mysql to influxDB

---
+++ mysqlToInfluxDB/src/main/java/batch/InfluxdbItemWriter.java
@@ -0,0 +1,42 @@
+package batch;
+
+import java.util.List;
+
+import org.springframework.batch.item.ItemWriter;
+
+import influxDB.InfluxDBConn;
+import vo.DatasetVO;
+
+public class InfluxdbItemWriter implements ItemWriter<DatasetVO> {
+	private final static String IP_ADDR = "localhost";
+	private final static String PORT = "8086";
+	private final static String USER = "root";
+	private final static String PASSWORD = "root";
+	
+	private final static String DATABASE = "Statistics";		
+	private final static String MEASUREMENT = "test16"; 
+	
+	InfluxDBConn db = new InfluxDBConn(IP_ADDR,PORT,DATABASE,USER,PASSWORD);
+
+	
+	public void write(List<? extends DatasetVO> items) throws Exception {
+		db.setUp();
+
+		for (DatasetVO item : items) {
+			DatasetVO datasetVO = new DatasetVO();
+
+			datasetVO.setCategoryId(item.getCategoryId());
+			datasetVO.setCategoryName(item.getCategoryName());
+			datasetVO.setMobileServiceYn(item.getMobileServiceYn());
+			datasetVO.setObjectId(item.getObjectId());
+			datasetVO.setObjectName(item.getObjectName());
+			datasetVO.setPcServiceYn(item.getPcServiceYn());
+			datasetVO.setPerson(item.getPerson());
+
+			db.customWrite(MEASUREMENT,datasetVO);		//measurement, VO, time
+		}
+		
+		db.closeDB();
+	}
+
+}
