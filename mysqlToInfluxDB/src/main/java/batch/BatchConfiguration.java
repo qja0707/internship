b31aaaa61cdcbb4c1e57a62c664d6dda4c60cac3
gyubeom.park <gyubeom.park@navercorp.com>
2017-07-24 10:25:40 +0900

    change external ip -> local ip

--- mysqlToInfluxDB/src/main/java/batch/BatchConfiguration.java
+++ mysqlToInfluxDB/src/main/java/batch/BatchConfiguration.java
@@ -38,7 +38,7 @@ public class BatchConfiguration {
 	public DataSource dataSource() {
 		final DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/dbForBatch?useSSL=false");
+		dataSource.setUrl("jdbc:mysql://localhost:13306/dbForBatch?useSSL=false");
 		dataSource.setUsername("root");
 		dataSource.setPassword("!@#123");
 		
